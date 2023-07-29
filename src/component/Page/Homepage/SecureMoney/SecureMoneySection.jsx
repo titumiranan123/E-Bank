078697b5e13dbf50a93049a436530521c250f85a
MD Titumir Anan <titumiranan.gtc@gmail.com>
2023-07-29 17:47:15 +0600

    secureFinance section done

--- src/component/Page/Homepage/SecureMoney/SecureMoneySection.jsx
+++ src/component/Page/Homepage/SecureMoney/SecureMoneySection.jsx
@@ -1,6 +1,9 @@
-import { FaHeading } from "react-icons/fa";
-import Servicecard from "../../ServiceCard/Servicecard";
 
+import Heading from "../../Heading/Heading";
+import SecureFinanceCard from "../../Secure&financeCard/SecureFinanceCard";
+import img1 from '../../../../assets/trade.jpg'
+import img2 from '../../../../assets/mutal.jpg'
+import img3 from '../../../../assets/multicurency.jpg'
 
 const SecureMoneySection = () => {
     const title = 'We Offer Banking For EveryOne'
@@ -8,12 +11,14 @@ const SecureMoneySection = () => {
     const mainHeading = ' Secure Your Money '
     return (
 
-        <div>
+        <div >
             <div>
-                <FaHeading title={title} mainHeading={mainHeading} subHeading={subHeading} />
+                <Heading title={title} mainHeading={mainHeading} subHeading={subHeading} />
             </div>
-            <div>
-
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+                <SecureFinanceCard title={'Trades FX'} image={img1} />
+                <SecureFinanceCard title={'Mutual Funds'} image={img2} />
+                <SecureFinanceCard title={"Multi Currency AC's"} image={img3} />
             </div>
 
         </div>
